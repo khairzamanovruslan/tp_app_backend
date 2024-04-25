b53e429524b09081f034603c1ecc1f1ec2366b5f
Ruslan <khrg1993@mail.ru>
2024-04-25 22:31:07 +0500

    редактировал админ панель

--- app/admin/views.py
+++ app/admin/views.py
@@ -8,19 +8,27 @@ class UsersAdmin(ModelView, model=Users):
     name_plural = "Users App"
     name = 'User App'
     column_list = [c.name for c in Users.__table__.c if c.name != 'hashed_password' and c.name != 'id']
-    column_details_exclude_list = [Users.hashed_password, Users.id]
+    column_searchable_list = [Users.email]
+    column_details_exclude_list = [Users.hashed_password]
+    can_edit = False
+    can_delete = False
     page_size = 50
 
 class UsersTGAdmin(ModelView, model=UsersTG):
     name_plural = "Users Telegram Bot"
     name = 'User Telegram Bot'
     column_list = [c.name for c in UsersTG.__table__.c if c.name != 'id']
+    column_searchable_list = [UsersTG.full_name]
+    column_sortable_list = [UsersTG.full_name]
+    save_as = False
     page_size = 50
 
 class SubstationAdmin(ModelView, model=Substation):
     name_plural = "Substations"
     name = 'Substation'
     column_list = [c.name for c in Substation.__table__.c if c.name != 'id']
+    column_searchable_list = [Substation.name]
+    column_sortable_list = [Substation.name]
     page_size = 150
 
    
