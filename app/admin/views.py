0edd7683522abac20f5f84aa426af6917d34cf16
Ruslan <khrg1993@mail.ru>
2024-05-13 14:46:55 +0500

    у добавления tp, сделал поля по дефолку пустаая строка

--- app/admin/views.py
+++ app/admin/views.py
@@ -7,12 +7,14 @@ from app.users_tg.models import UsersTG
 class UsersAdmin(ModelView, model=Users):
     name_plural = "Users App"
     name = 'User App'
-    column_list = [c.name for c in Users.__table__.c if c.name != 'hashed_password' and c.name != 'id']
+    column_list = [c.name for c in Users.__table__.c if c.name !=
+                   'hashed_password' and c.name != 'id']
     column_searchable_list = [Users.email]
     column_details_exclude_list = [Users.hashed_password]
     can_edit = False
     can_delete = False
     page_size = 50
+
 
 class UsersTGAdmin(ModelView, model=UsersTG):
     name_plural = "Users Telegram Bot"
@@ -23,6 +25,7 @@ class UsersTGAdmin(ModelView, model=UsersTG):
     save_as = False
     page_size = 50
 
+
 class SubstationAdmin(ModelView, model=Substation):
     name_plural = "Substations"
     name = 'Substation'
@@ -30,5 +33,3 @@ class SubstationAdmin(ModelView, model=Substation):
     column_searchable_list = [Substation.name]
     column_sortable_list = [Substation.name]
     page_size = 150
-
-   
--- app/substation/router.py
+++ app/substation/router.py
@@ -9,9 +9,9 @@ router = APIRouter(prefix='/substation', tags=['ТП'])
 
 @router.post('')
 async def create_substation(
-    substation_data: SSubstation_NameCoordinates, 
+    substation_data: SSubstation_NameCoordinates,
     user_access: Users = Depends(get_current_user)
-    ) -> SSubstation_NameCoordinates:
+) -> SSubstation_NameCoordinates:
     """Создать ТП"""
     existing_substation = await SubstationDAO.find_one_or_none(name=substation_data.name)
     if existing_substation:
@@ -19,8 +19,8 @@ async def create_substation(
             status_code=409, detail="Такая ТП уже существует")
     new_substation = await SubstationDAO.add(
         name=substation_data.name,
-        latitude=substation_data.latitude,
-        longitude=substation_data.longitude
+        latitude=substation_data.latitude or '',
+        longitude=substation_data.longitude or ''
     )
     return new_substation
 
@@ -33,38 +33,40 @@ async def get_substations(user_access: Users = Depends(get_current_user)) -> lis
     list_substation = []
     for item in substations_data:
         item = dict(item)
-        link = {"link": f'https://yandex.ru/maps/?pt={item['longitude']},{item['latitude']}&z=18&l=map'}
+        link = {
+            "link": f'https://yandex.ru/maps/?pt={item['longitude']},{item['latitude']}&z=18&l=map'}
         item.update(link)
         list_substation.append(item)
     return list_substation
 
 
 @router.get('/{name}')
-async def get_substation(name: str, user_access: Users = Depends(get_current_user))-> SSubstation_FullData:
+async def get_substation(name: str, user_access: Users = Depends(get_current_user)) -> SSubstation_FullData:
     """Получить ТП"""
     substation_data: SSubstation_FullData = await SubstationDAO.find_one_or_noneDAO(name=name)
     if not substation_data:
         raise HTTPException(
-            status_code=409, detail= "Такая ТП отсутствует")
+            status_code=409, detail="Такая ТП отсутствует")
     substation = dict(substation_data)
-    link = {"link": f'https://yandex.ru/maps/?pt={substation['longitude']},{substation['latitude']}&z=18&l=map'}
+    link = {"link": f'https://yandex.ru/maps/?pt={
+        substation['longitude']},{substation['latitude']}&z=18&l=map'}
     substation.update(link)
     return substation
 
 
 @router.put('/{name}')
 async def update_substation(
-    name: str, 
-    substation_data: SSubstation_Coordinates, 
-    user_access: Users = Depends(get_current_user)) -> bool:
+        name: str,
+        substation_data: SSubstation_Coordinates,
+        user_access: Users = Depends(get_current_user)) -> bool:
     """Обновить ТП"""
     existing_substation = await SubstationDAO.find_one_or_none(name=name)
     if not existing_substation:
         raise HTTPException(
             status_code=404, detail="Такая ТП отсутствует")
     update_substation = await SubstationDAO.update_dao(
-        name, 
-        latitude=substation_data.latitude, 
+        name,
+        latitude=substation_data.latitude,
         longitude=substation_data.longitude)
     print('put', update_substation)
     return True
